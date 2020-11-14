bb4162b16b4a8f595d30107d66f8623796f4c7f2
bedrantirak <bdrntrk@gmail.com>
2020-11-14 12:18:48 +0300

    Update App.js

--- movieapp/src/App.js
+++ movieapp/src/App.js
@@ -1,8 +1,25 @@
+import React , {useEffect,useState} from 'react'
+import Movie from "./components/Movie";
 
-function App() {
+   const FEATURED_API = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=5c3b6d793805cf5c92a13066fd419c99&page=1";
+   
+
+   const SEARCH_API = "https://api.themoviedb.org/3/search/movie?&api_key=5c3b6d793805cf5c92a13066fd419c99&query=";
+
+   function App() {
+    const [movies, setMovies] = useState([]);
+
+    useEffect( ()=>{
+    fetch(FEATURED_API).then(res => res.json()).then(data =>{
+     console.log(data);
+      setMovies(data.results);
+    });
+  },[]);
   return (
-    <div className="App">
-     
+    <div className="movie-container">
+     {movies.length > 0 && movies.map(movie =>(
+       <Movie key={movie.id} {...movie}/>
+     ))}
     </div>
   );
 }
